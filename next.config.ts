7fb0d20663f61cb6e2ab305272b43bd4737527e3
umit-akdeniz <umitakdenizjob@gmail.com>
2025-03-21 02:53:24 +0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['via.placeholder.com'], // Kullandığın harici domain’i ekle
+    domains: ['via.placeholder.com'], 
+  },
+  reactStrictMode: true,
+  env: {
+    CONTENTFUL_SPACE_ID:process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET,
+    CONTENTFUL_REVALIDATE_SECRET: process.env.CONTENTFUL_REVALIDATE_SECRET,
+    CONTENTFUL_MANAGEMENT_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
   },
 };
-
-module.exports = nextConfig;
-
-export default nextConfig;
